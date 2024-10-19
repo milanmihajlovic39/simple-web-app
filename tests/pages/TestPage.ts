c19b5bdfa4c80a21d9ceb5a37dff9f63009e13f1
Milan Mihajlovic <milanmihajlovic39@gmail.com>
2024-10-19 18:44:13 +0200

    update tests

---
+++ tests/pages/TestPage.ts
@@ -0,0 +1,23 @@
+import { Locator, Page } from '@playwright/test';
+
+export class TestPage {
+    readonly page: Page;
+    readonly firstNameField: Locator;
+    readonly ageField: Locator;
+    readonly isStudentCheckbox: Locator;
+    readonly applyDataButton: Locator;
+    readonly displayFirstNameLabel: Locator;
+    readonly displayAgeLabel: Locator;
+    readonly displayIsStudentLabel: Locator;
+  
+    constructor(page: Page) {
+      this.page = page;
+      this.firstNameField = page.locator('#firstName');
+      this.ageField = page.locator('#age');
+      this.isStudentCheckbox = page.locator('#isStudent');
+      this.applyDataButton = page.locator('#applyData');
+      this.displayFirstNameLabel = page.locator('#displayFirstName');
+      this.displayAgeLabel = page.locator('#displayAge');
+      this.displayIsStudentLabel = page.locator('#displayIsStudent');
+    }
+}
